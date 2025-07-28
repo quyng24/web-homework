67b8f479eedc268f736d8a2ced54cf1e03457848
NGUYEN VAN QUY <nquy50771@lgcns.com>
2025-07-28 09:36:27 +0700

    complete user dashboard

---
+++ fe/src/pages/user/UserTopic.jsx
@@ -0,0 +1,11 @@
+import LayoutDefault from "../../layouts/LayoutDefault";
+
+export default function UserTopic() {
+  return (
+    <LayoutDefault>
+        <div className="flex justify-center items-center">
+        topic
+        </div>
+    </LayoutDefault>
+  )
+}
